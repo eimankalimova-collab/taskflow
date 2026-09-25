cf205f96f39ed27982aac54dbf1bce89ba6c4a0b
eimankalimova-collab <eimankalimova-collab@users.noreply.github.com>
2026-09-25 15:51:29 +0300

    Резервная копия в Google одним нажатием

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "847732624" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1215262591" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
