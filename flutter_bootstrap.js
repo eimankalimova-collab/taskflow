2ce08f682772f78c82ff619796d5c86ddbb1be7a
eimankalimova-collab <eimankalimova-collab@users.noreply.github.com>
2026-09-25 23:35:39 +0300

    Публикация только для автора: ключ публикации

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1726589727" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "718424060" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
