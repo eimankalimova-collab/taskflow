e36a0a9948ccc97be1936819bec656edf4553885
eimankalimova-collab <eimankalimova-collab@users.noreply.github.com>
2026-09-25 14:39:21 +0300

    Импорт показывает итог (проекты/задачи/участники)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "838760919" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "861511304" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
